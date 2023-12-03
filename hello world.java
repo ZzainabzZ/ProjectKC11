1af9a9b3f3cc14373537e03bcdd794fbeaf89682
ZzainabzZ <zafridi6010@students.mc3.edu>
2023-12-03 18:53:10 -0500

    Create hello world.java

---
+++ hello world.java
@@ -0,0 +1,7 @@
+                                                                                                          ******/
+import java.util.Date;
+
+/**
+ * This class represents a Character for my favorite book series: Throne of Glass.
+ */
+public class Character 
