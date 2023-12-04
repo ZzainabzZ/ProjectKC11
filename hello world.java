7127367936d409219d7c85274e201a311d761a0a
ZzainabzZ <zafridi6010@students.mc3.edu>
2023-12-03 19:00:55 -0500

    Delete hello world.java

--- hello world.java
+++
@@ -1,7 +0,0 @@
-                                                                                                          ******/
-import java.util.Date;
-
-/**
- * This class represents a Character for my favorite book series: Throne of Glass.
- */
-public class Character 
