396542593c6eccba1ff3e7c4c3731225736c4b8e
ZzainabzZ <zafridi6010@students.mc3.edu>
2023-12-03 19:09:55 -0500

    HelloAgain
    Here is an old piece of simple code that brings out "Hey, my name is Zainab Afridi"

---
+++ HelloAgain.java
@@ -0,0 +1,30 @@
+                                          /******
+                                                 Name: Zainab Afridi
+                                                 Date: 9/8/2022
+                                                 Purpose: Lab#2_HelloWorld
+                                                 repurpose: reusing this as
+                                                   github repository push
+                                                                          ******/
+
+public class HelloAgain
+{
+      public static void main(String[] args)
+      {
+      
+         // program starts -> 
+         // storing a message in variable "intro"
+         String intro = "Hey, my name is Zainab Afridi";
+         
+         // printing message (outputting)
+         System.out.println(intro);
+         
+                  
+      
+      
+      }
+
+
+
+
+
+}
---
+++ hello world.java
@@ -0,0 +1,7 @@
+private class HelloAgain
+{
+  public static void main(String[] args) 
+   {  
+      system.println("Hello Again");
+   }
+}
